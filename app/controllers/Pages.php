04025ec6090582de27d29a17e08bcc4d17bf8054
Jakub Kołuda <jko01@outlook.com>
2023-07-17 18:41:46 +0200

    Add layout, change pages

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,14 +4,20 @@ class Pages extends Controller
 {
   public function index()
   {
-    $data = ['title' => 'Posts App',];
+    $data = [
+      'title' => 'Posts App',
+      'description' => 'Simple social network build on the simple MVC PHP Framework'
+    ];
 
     $this->view('pages/index', $data);
   }
 
   public function about()
   {
-    $data = ['title' => 'About Us'];
+    $data = [
+      'title' => 'About Us',
+      'description' => 'App to share posts with other users'
+    ];
     $this->view('pages/about', $data);
   }
 }
