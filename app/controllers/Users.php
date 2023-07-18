ad782d03e31ba988c3e6a054dbdd0fbddfac0930
Jakub Kołuda <jko01@outlook.com>
2023-07-18 19:21:16 +0200

    Add register and login form views

---
+++ app/controllers/Users.php
@@ -0,0 +1,38 @@
+<?php
+
+class Users extends Controller
+{
+  public function __construct()
+  {
+  }
+  public function register()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    } else {
+      $data = [
+        'name' => '',
+        'email' => '',
+        'password' => '',
+        'confirm_password' => '',
+        'name_err' => '',
+        'email_err' => '',
+        'password_err' => '',
+        'confrim_password_err' => ''
+      ];
+      $this->view('users/register', $data);
+    }
+  }
+  public function login()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    } else {
+      $data = [
+        'email' => '',
+        'password' => '',
+        'email_err' => '',
+        'password_err' => '',
+      ];
+      $this->view('users/login', $data);
+    }
+  }
+}
