51177efb34aa89e221588c87f7137a08695aa297
Jakub Kołuda <jko01@outlook.com>
2023-07-19 19:43:40 +0200

    Add form validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,6 +8,46 @@ class Users extends Controller
   public function register()
   {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+
+      $data = [
+        'name' => trim($_POST['name']),
+        'email' => trim($_POST['email']),
+        'password' => trim($_POST['password']),
+        'confirm_password' => trim($_POST['confirm_password']),
+        'name_err' => '',
+        'email_err' => '',
+        'password_err' => '',
+        'confrim_password_err' => ''
+      ];
+      if(empty($data['email'])) {
+        $data['email_err'] = 'Please enter email';
+      }
+      
+      if(empty($data['name'])) {
+        $data['name_err'] = 'Please enter name';
+      }
+      
+      if(empty($data['password'])) {
+        $data['password_err'] = 'Please enter password';
+      } elseif(strlen($data['password']) < 6) {
+        $data['password_err'] = 'Password must be at least 6 characters';
+      }
+            
+      if(empty($data['confirm_password'])) {
+        $data['confirm_password_err'] = 'Please confrim password';
+      } else {
+        if($data['password'] !== $data['confirm_password']) {
+          $data['confirm_password_err'] = 'Passwords do not match';
+        }
+      }
+      if(empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+        die("Success");
+      } else {
+        $this->view('users/register', $data);
+      }
+
     } else {
       $data = [
         'name' => '',
@@ -25,6 +65,26 @@ class Users extends Controller
   public function login()
   {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $data = [
+        'email' => trim($_POST['email']),
+        'password' => trim($_POST['password']),
+        'email_err' => '',
+        'password_err' => '',
+      ];
+
+      if(empty($data['email'])) {
+        $data['email_err'] = 'Please enter email';
+      }
+      if(empty($data['password'])) {
+        $data['password_err'] = 'Please enter password';
+      }
+
+      if(empty($data['email_err']) && empty($data['password_err'])) {
+        die("success");
+      } else {
+        $this->view('users/login', $data);
+      }
+
     } else {
       $data = [
         'email' => '',
